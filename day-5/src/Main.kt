3a0ec17815ef4946515c59d1f424d0f5803e3e32
Jussi <jkallio@gmail.com>
2019-12-07 12:22:31 +0200

    Day-5: Found solution (Part-2)

--- day-5/src/Main.kt
+++ day-5/src/Main.kt
@@ -71,69 +71,55 @@ import java.io.File
  *
  * After providing 1 to the only input instruction and passing all the tests, what diagnostic code does the program
  * produce?
+ *
+ *
+ * --- Part Two ---
+ * The air conditioner comes online! Its cold air feels good for a while, but then the TEST alarms start to go off.
+ * Since the air conditioner can't vent its heat anywhere but back into the spacecraft, it's actually making the air
+ * inside the ship warmer.
+ *
+ * Instead, you'll need to use the TEST to extend the thermal radiators. Fortunately, the diagnostic program
+ * (your puzzle input) is already equipped for this. Unfortunately, your Intcode computer is not.
+ *
+ * Your computer is only missing a few opcodes:
+ *      - Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value
+ *         from the second parameter. Otherwise, it does nothing.
+ *      - Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from
+ *          the second parameter. Otherwise, it does nothing.
+ *      - Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position
+ *          given by the third parameter. Otherwise, it stores 0.
+ *      - Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given
+ *          by the third parameter. Otherwise, it stores 0.
+ *
+ * Like all instructions, these instructions need to support parameter modes as described above.
+ *
+ * Normally, after an instruction is finished, the instruction pointer increases by the number of values in that instruction. However, if the instruction modifies the instruction pointer, that value is used and the instruction pointer is not automatically increased.
+ *
+ * For example, here are several programs that take one input, compare it to the value 8, and then produce one output:
+ *      3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+ *      3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+ *      3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+ *      3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+ *
+ * Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+ *      3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 (using position mode)
+ *      3,3,1105,-1,9,1101,0,0,12,4,12,99,1 (using immediate mode)
+ *
+ * Here's a larger example:
+ *      3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+ *      1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+ *      999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+ *
+ * The above example program uses an input instruction to ask for a single number. The program will then output 999 if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is greater than 8.
+ *
+ * This time, when the TEST diagnostic program runs its input instruction to get the ID of the system to test, provide it 5, the ID for the ship's thermal radiator controller. This diagnostic test suite only outputs one number, the diagnostic code.
+ *
+ * What is the diagnostic code for system ID 5?
 */
-
-fun runIntCode(intcode: Array<String>) : Int {
-    var i = 0
-    var lastOutput = 0
-    loop@ while (i < intcode.count()) {
-        val opcode = intcode[i]
-        val op = if (opcode.length > 2) opcode.substring(opcode.length-2).toInt() else opcode.toInt()
-        val mode1 = if (opcode.length > 2) opcode.substring(opcode.length-3, opcode.length-2).toInt() else 0
-        val mode2 = if (opcode.length > 3) opcode.substring(opcode.length-4, opcode.length-3).toInt() else 0
-        val mode3 = if (opcode.length > 4) opcode.substring(opcode.length-5, opcode.length-4).toInt() else 0
-
-        when (op) {
-            1,2 -> { // op = 'add' / 'multiply'
-                val a = if (mode1 == 1) intcode[i+1].toInt() else intcode[intcode[i+1].toInt()].toInt()
-                val b = if (mode2 == 1) intcode[i+2].toInt() else intcode[intcode[i+2].toInt()].toInt()
-                assert(mode3 == 0) // Writing operations  must always be 'position mode'
-                val storePos = intcode[i+3].toInt()
-                val result = if (op == 1) a + b else a * b
-                intcode[storePos] = result.toString()
-                i += 4
-            }
-
-            3 -> { // op = 'input'
-                assert(mode1 == 0 && mode2 == 0 && mode3 == 0)
-                print("Your input > ")
-                val strInput = readLine()!!
-                //val strInput = "1" // Always input '1'
-
-                val input = strInput.toIntOrNull()
-                if (input == null) {
-                    println ("*** Invalid user input ***")
-                    return -1
-
-                }
-
-                val storePos = intcode[i + 1].toInt()
-                intcode[storePos] = input.toString()
-                i += 2
-            }
-
-            4 -> { // op = 'output'
-                lastOutput = if (mode1 == 1) intcode[i+1].toInt() else intcode[intcode[i+1].toInt()].toInt()
-                println("Output: $lastOutput")
-                i += 2
-            }
-
-            99 -> {
-                break@loop
-            }
-
-            else -> {
-                println ("*** Critical failure ***")
-                return -1
-            }
-        }
-    }
-    return lastOutput
-}
 
 fun main(args: Array<String>) {
 
-    if (args.count() != 1)  {
+    if (args.count() != 1) {
         print("Missing argument: <filename>")
         return
     }
@@ -143,9 +129,10 @@ fun main(args: Array<String>) {
     File(args[0]).forEachLine { input += it }
 
     // Split comma separated integers into a list
-    val intcode = mutableListOf<String>()
-    input.split(',').forEach { intcode.add(it) }
-    val runResult = runIntCode(intcode.toTypedArray())
-    println("Intcode result = $runResult")
+    val tokens = mutableListOf<String>()
+    input.split(',').forEach { tokens.add(it) }
 
+    val intcode = Intcode(tokens.toTypedArray())
+    val result = intcode.run()
+    println("Intcode result = $result")
 }
